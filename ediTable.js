25ff77039faea3da0d5e333cdf708c00859ab9b4
Scanny <scannygec@gmail.com>
2013-07-03 16:17:53 +0530

    Hidden forms

--- ediTable.js
+++ ediTable.js
@@ -2,6 +2,21 @@
     $.fn.ediTable = function(options) {
         var settings = $.extend({
             attributes: null
-        },options)
+        },options);
+
+        return this.each(function(){
+            var $rows = $(this).find('tbody').find('tr');
+            $.each($rows, function(index,$row){
+                var $form = $('<form>').attr({
+                    style: 'display: none;'
+                });
+                $.each(settings.attributes,function(index,value){
+                    $('<input>').attr({
+                        name: value
+                    }).appendTo($form);
+                });
+                $form.insertAfter($row);
+            })
+        })
     }
 })(jQuery);

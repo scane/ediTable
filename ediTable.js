517a695167b454234be62a9df3b6f91d2c1aa89a
Scanny <scannygec@gmail.com>
2013-07-04 12:06:31 +0530

    Adds update action

--- ediTable.js
+++ ediTable.js
@@ -1,21 +1,57 @@
 (function($) {
     $.fn.ediTable = function(options) {
         var settings = $.extend({
-            attributes: null
+            attributes: null,
+            updateURL: null,
+            onUpdate: null
         },options);
 
+        $('.ediTable-edit').on('click',function(){
+            var $row = $(this).parents('tr');
+            var $cols = $row.find('td[class!="action"]');
+            $.each($cols,function(index,col){
+                var $input = $('<input>').attr({name: settings.attributes[index], type: 'text', value: $(col).html() });
+                $(col).html($input);
+            })
+            $row.find('.ediTable-save').attr('disabled',false)
+            $(this).attr('disabled',true)
+        });
+
+        $('.ediTable-save').on('click',function(){
+            var $row = $(this).parents('tr');
+            if( settings.updateURL ) {
+                $.ajax(
+                    {
+                        url: settings.updateURL,
+                        data: $row.find('input[type="text"]').serialize(),
+                        success: function() {
+                            $row.find('.ediTable-edit').attr('disabled',false);
+                            $(this).attr('disabled',true)
+                            if($.isFunction( settings.onUpdate ))
+                                settings.onUpdate.call()
+                        },
+                        type: 'post'
+                    }
+                )
+            }
+        })
+
         return this.each(function(){
-            var $rows = $(this).find('tbody').find('tr');
-            $.each($rows, function(index,$row){
-                var $form = $('<form>').attr({
-                    style: 'display: none;'
-                });
-                $.each(settings.attributes,function(index,value){
-                    $('<input>').attr({
-                        name: value
-                    }).appendTo($form);
-                });
-                $form.insertAfter($row);
+            var $rows = $(this).find('tbody tr');
+            $.each($rows, function(index,row){
+                var id = $(row).data('id')
+                $(row).find('.ediTable-edit, .ediTable-save').attr('data-id',id);
+//                var form = $('<form>').attr({
+//                    id: 'ediTable-form' + id,
+//                    style: 'display: none;'
+//                });
+//                $.each(settings.attributes,function(index,value){
+//                    $('<input>').attr({
+//                        name: value
+//                    }).appendTo(form);
+//                });
+//                form.insertAfter(row);
+                $('.ediTable-save').attr('disabled',true)
             })
         })
     }

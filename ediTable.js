c1ba7d1496d18cbc3d1a49b34d55e5e5a16ce745
Scanny <scannygec@gmail.com>
2013-07-05 17:42:08 +0530

    Adds checkbox support

--- ediTable.js
+++ ediTable.js
@@ -1,7 +1,7 @@
 (function($) {
     $.fn.ediTable = function(options) {
         var settings = $.extend({
-            attributes: null,
+            fields: null,
             onUpdate: null
         },options);
 
@@ -9,8 +9,18 @@
             var $row = $(this).parents('tr');
             var $cols = $row.find('td[class!="action"]');
             $.each($cols,function(index,col){
-                var $input = $('<input>').attr({name: settings.attributes[index], type: 'text', value: $(col).html() });
-                $(col).html($input);
+                var value = $(col).html();
+                $(col).html('');
+                var field = settings.fields[index]
+                var $input = $('<input>').attr({name: field.name, type: 'text' , value: value });
+                if('type' in field)
+                    switch(field.type){
+                        case 'checkbox':
+                            $input.attr({type: 'checkbox', value:true, checked: field.labels[0] == value ? true : false})
+                            var hidden_field = $('<input>').attr({name: field.name, type:'hidden',value: false });
+                            $(col).append(hidden_field)
+                    }
+                $(col).append($input);
             })
             $row.find('.ediTable-save').attr('disabled',false);
             $(this).attr('disabled',true);
@@ -22,12 +32,21 @@
             $.ajax(
                 {
                     url: $row.data('update-url'),
-                    data: $row.find('input[type="text"]').serialize(),
+                    data: $row.find('input').serialize(),
                     success: function(response) {
                         var $cols = $row.find('td[class!="action"]');
                         $.each($cols,function(index,col){
-                            var input = $(col).find('input');
-                            input.replaceWith(input.val())
+                            var input = $(col).find('input[type!="hidden"]');
+                            var value = '';
+                            var field = settings.fields[index];
+                            switch(input.attr('type')) {
+                                case 'checkbox':
+                                    input.is(':checked') ? value = field.labels[0] : value = field.labels[1];
+                                    break;
+                                default:
+                                    value = input.val();
+                            }
+                            input.replaceWith(value)
                         })
                         $row.find('.ediTable-edit').attr('disabled',false);
                         save_button.attr('disabled',true);
